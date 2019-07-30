2dabdbe704f1a94bb7fc935cc025b32b17e7585c
lzpol <lzpolol@163.com>
2019-07-30 09:32:03 +0800

    tijiao

--- src/pages/monitoring/exceptionRecord/index.js
+++ src/pages/monitoring/exceptionRecord/index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import moment from 'moment';
-import ReactEcharts from 'echarts-for-react';
+// import ReactEcharts from 'echarts-for-react';
 import {
     Button,
     Input,
@@ -13,20 +13,51 @@ import {
     Tooltip,
     Select, Modal, Tag, Divider, Dropdown, Icon, Menu, Popconfirm, message, Empty, Switch,
 } from 'antd';
-import { PointIcon } from '@/utils/icon'
-import { routerRedux } from 'dva/router';
-import { connect } from 'dva';
-import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import styles from './index.less';
-import NavigationTree from '../../../components/NavigationTree'
-import RecordEchartTable from '../../../components/recordEchartTable'
+// import { PointIcon } from '@/utils/icon'
+// import { routerRedux } from 'dva/router';
+// import { connect } from 'dva';
+// import { PageHeaderWrapper } from '@ant-design/pro-layout';
+// import styles from './index.less';
+// import NavigationTree from '../../../components/NavigationTree'
+// import RecordEchartTable from '../../../components/recordEchartTable'
 
-
+const data = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
 // @connect(({ loading, exceptionrecord }) => ({
    
 // }))
 
-class Index extends Component {
+class ExceptionRecord extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -45,17 +76,21 @@ class Index extends Component {
       
 
         return (
-            <div id="record">
-                <PageHeaderWrapper>
-                    <RecordEchartTable  />
-                </PageHeaderWrapper>
-                <NavigationTree domId="#record" choice={false} onItemClick={value => {
-                    // if (value.length > 0 && !value[0].IsEnt) {
-                    //     this.changeDgimn(value[0].key)
-                    // }
-                }} />
-            </div>
+            // <div id="record">
+            //     <PageHeaderWrapper>
+            //         <RecordEchartTable  />
+            //     </PageHeaderWrapper>
+            //     <NavigationTree domId="#record" choice={false} onItemClick={value => {
+            //         // if (value.length > 0 && !value[0].IsEnt) {
+            //         //     this.changeDgimn(value[0].key)
+            //         // }
+            //     }} />
+            // </div>
+            <Table
+            column={columns}
+            dataSource={data}
+            ></Table>
         );
     }
 }
-export default Index;
+export default ExceptionRecord;
