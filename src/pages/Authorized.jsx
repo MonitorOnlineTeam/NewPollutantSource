5619dd34699f4c201c41f39ce2af39723e7f7f3c
wjwgit <w417287508@163.com>
2019-08-05 11:25:15 +0800

    update

--- src/pages/Authorized.jsx
+++ src/pages/Authorized.jsx
@@ -34,11 +34,11 @@ const AuthComponent = ({
 }) => {
   const { currentUser } = user;
   const { routes = [] } = route;
-  const isLogin = currentUser && currentUser.name;
+  const isLogin = currentUser && currentUser.UserName;
   return (
     <Authorized
       authority={getRouteAuthority(location.pathname, routes) || ''}
-      // noMatch={isLogin ? <Redirect to="/exception/403" /> : <Redirect to="/user/login" />}
+      noMatch={isLogin ? <Redirect to="/exception/403" /> : <Redirect to="/user/login" />}
       noMatch={<Redirect to="/user/login" />}
     >
       {children}
