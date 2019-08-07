a5e20f349253b3caac40b248a66b4f5598e9be54
wjwgit <w417287508@163.com>
2019-08-07 15:52:22 +0800

    update

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 /* eslint no-useless-escape:0 import/prefer-default-export:0 */
-import { Icon, Badge, Popover } from 'antd';
+import { Icon, Badge, Popover,message } from 'antd';
 import moment from 'moment';
 
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
