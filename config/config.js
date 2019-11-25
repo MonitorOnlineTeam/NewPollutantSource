1b75abbab1edb60e98c048aed7e905d0caf3a3db
lzpol <lzpolol@163.com>
2019-11-25 11:48:04 +0800

    update

--- config/config.js
+++ config/config.js
@@ -845,12 +845,12 @@ export default {
                   path: '/qualityControl/operationRecords',
                   component: './qualityControl/operationRecords',
                 },
-                // 质控仪维护记录
-                {
-                  name: 'maintainRecord',
-                  path: '/qualityControl/maintainRecord',
-                  component: './qualityControl/maintainRecord',
-                },
+                // // 质控仪维护记录
+                // {
+                //   name: 'maintainRecord',
+                //   path: '/qualityControl/maintainRecord',
+                //   component: './qualityControl/maintainRecord',
+                // },
               ]
             },
             /* 任务详情 */
