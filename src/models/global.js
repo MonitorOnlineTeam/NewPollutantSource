4c3e6d4955d71b59b681bd84516a9fb49b69089d
IoriCnn <chengeng0502@126.com>
2019-12-09 11:50:16 +0800

    修改推送

--- src/models/global.js
+++ src/models/global.js
@@ -326,6 +326,7 @@ export default Model.extend({
       return mywebsocket.listen(data => {
         // 实时数据："{"MessageType":"RealTimeData","Message":[{"DGIMN":"201809071401","PollutantCode":"s01","MonitorTime":"2018-11-21 01:22:41","MonitorValue":36.630,"MinStrength":null,"MaxStrength":null,"CouStrength":null,"IsOver":-1,"IsException":0,"Flag":"","ExceptionType":"","AlarmLevel":"身份验证失败","AlarmType":"无报警","Upperpollutant":"0","Lowerpollutant":"0","PollutantResult":"","AlarmTypeCode":0,"StandardColor":"red","StandardValue":"-","OverStandValue":"","DecimalReserved":3}]}"
         const obj = JSON.parse(data);
+        debugger;
         console.log('real=', obj)
         switch (obj.MessageType) {
           case 'RealTimeData':
--- src/utils/mywebsocket.js
+++ src/utils/mywebsocket.js
@@ -1,9 +1,17 @@
 import Cookie from 'js-cookie';
 import config from '../config';
 
-const ip = isInnerIPFn();
+// const ip = isInnerIPFn();
 // cg add 2018.4.1
-const ws = new WebSocket(`ws://${ip}/`);
+const ws = getWebSocket(config.webSocketPushURL.split(',')[0],config.webSocketPushURL.split(',')[1]);
+function getWebSocket(innerIp, outIp) {
+  let wss = new WebSocket(`ws://${innerIp}/`);
+  if(!innerIp){
+    wss = new WebSocket(`ws://${outIp}/`);
+  }
+  debugger;
+  return wss;
+}
 try {
   window.ws = ws;
 } catch (e) {
@@ -50,68 +58,59 @@ export function listen(callback) {
   }
 }
 
-/*判断是否是内网IP*/
+/* 判断是否是内网IP */
 function isInnerIPFn() {
-  var returnIP = "";
-  var innerIp = config.webSocketPushURL.split(',')[0];
-  var outIp = config.webSocketPushURL.split(',')[1];
+  var returnIP = '';
+  debugger;
+  const innerIp = config.webSocketPushURL.split(',')[0];
+  const outIp = config.webSocketPushURL.split(',')[1];
   var returnIP = '';
   // 获取当前页面url
-  var curPageUrl = window.location.href;
+  let curPageUrl = window.location.href;
   if (curPageUrl.indexOf('localhost') !== -1) {
     returnIP = innerIp;
-  }
-  else {
-    var reg1 = /(http|ftp|https|www):\/\//g;//去掉前缀
+  } else {
+    const reg1 = /(http|ftp|https|www):\/\//g;// 去掉前缀
     curPageUrl = curPageUrl.replace(reg1, '');
     // console.log('curPageUrl-1  '+curPageUrl);
 
-    var reg2 = /\:+/g;//替换冒号为一点
+    const reg2 = /\:+/g;// 替换冒号为一点
     curPageUrl = curPageUrl.replace(reg2, '.');
     // console.log('curPageUrl-2  '+curPageUrl);
-    curPageUrl = curPageUrl.split('.');//通过一点来划分数组
+    curPageUrl = curPageUrl.split('.');// 通过一点来划分数组
     // console.log(curPageUrl);
 
     // console.log("curPageUrl=======", curPageUrl);
-    var ipAddress = curPageUrl[0] + '.' + curPageUrl[1] + '.' + curPageUrl[2] + '.' + curPageUrl[3];
+    const ipAddress = `${curPageUrl[0] }.${ curPageUrl[1] }.${curPageUrl[2] }.${curPageUrl[3]}`;
     // console.log("ipAddress=======", ipAddress);
-    var isInnerIp = false;//默认给定IP不是内网IP      
-    var ipNum = getIpNum(ipAddress);
-    var aBegin = getIpNum("10.0.0.0");
-    var aEnd = getIpNum("10.255.255.255");
-    var bBegin = getIpNum("172.16.0.0");
-    var bEnd = getIpNum("172.31.255.255");
-    var cBegin = getIpNum("192.168.0.0");
-    var cEnd = getIpNum("192.168.255.255");
-    var dBegin = getIpNum("127.0.0.0");
-    var dEnd = getIpNum("127.255.255.255");
-    isInnerIp = isInner(ipNum, aBegin, aEnd) || isInner(ipNum, bBegin, bEnd) || isInner(ipNum, cBegin, cEnd) || isInner(ipNum, dBegin, dEnd);
+    let isInnerIp = false;// 默认给定IP不是内网IP
+    const ipNum = getIpNum(ipAddress);
+    const aBegin = getIpNum('10.0.0.0');
+    const aEnd = getIpNum('10.255.255.255');
+    const bBegin = getIpNum('172.16.0.0');
+    const bEnd = getIpNum('172.31.255.255');
+    const dBegin = getIpNum('127.0.0.0');
+    const dEnd = getIpNum('127.255.255.255');
+    isInnerIp = isInner(ipNum, aBegin, aEnd) || isInner(ipNum, bBegin, bEnd) || isInner(ipNum, dBegin, dEnd);
     // console.log('是否是内网:' + isInnerIp);
     if (isInnerIp) {
       returnIP = innerIp;
-    }
-    else {
+    } else {
       returnIP = outIp;
     }
-
   }
   return returnIP;
 }
-function getIpNum(ipAddress) {/*获取IP数*/
-  var ip = ipAddress.split(".");
-  var a = parseInt(ip[0]);
-  var b = parseInt(ip[1]);
-  var c = parseInt(ip[2]);
-  var d = parseInt(ip[3]);
-  var ipNum = a * 256 * 256 * 256 + b * 256 * 256 + c * 256 + d;
+function getIpNum(ipAddress) { /* 获取IP数 */
+  const ip = ipAddress.split('.');
+  const a = parseInt(ip[0]);
+  const b = parseInt(ip[1]);
+  const c = parseInt(ip[2]);
+  const d = parseInt(ip[3]);
+  const ipNum = a * 256 * 256 * 256 + b * 256 * 256 + c * 256 + d;
   return ipNum;
 }
 
 function isInner(userIp, begin, end) {
   return (userIp >= begin) && (userIp <= end);
 }
-
-
-
-
-
