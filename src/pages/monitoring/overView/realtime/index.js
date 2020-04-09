c918b5c4242dd7493a08a3f9a6b6b6e935c3b1d5
Jiaqi <wangjq@polysoft.com.cn>
2020-04-09 11:06:24 +0800

    修改bug

--- src/pages/monitoring/overView/realtime/index.js
+++ src/pages/monitoring/overView/realtime/index.js
@@ -443,10 +443,10 @@ class index extends Component {
           }
           extra={
             <Radio.Group
-              defaultValue="data"
+              value="data"
               buttonStyle="solid"
               onChange={e => {
-                e.target.value === 'map' && router.push('/monitoring/mapview');
+                e.target.value === 'map' && router.push('/monitoring/mapview?tabName=数据一览 - 地图');
               }}
             >
               <Radio.Button value="data">数据</Radio.Button>
