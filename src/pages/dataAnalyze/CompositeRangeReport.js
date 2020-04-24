14e6ce714901b691571d495d6dec0ac81e68cd6f
Jiaqi <wangjq@polysoft.com.cn>
2020-04-24 17:44:36 +0800

    修改

--- src/pages/dataAnalyze/CompositeRangeReport.js
+++ src/pages/dataAnalyze/CompositeRangeReport.js
@@ -271,7 +271,7 @@ class CompositeRangeReport extends Component {
                 </Col>
               </Row>
             </Form>
-            <SdlTable columns={columns} dataSource={compositeRangeDataSource} pagination={false} defaultWidth={150}  />
+            <SdlTable columns={columns} dataSource={compositeRangeDataSource} pagination={false} defaultWidth={100}  />
             {/* scroll={{ x: 2600,y: 'calc(100vh - 380px)' }} */}
           </Card>
         </Spin>
