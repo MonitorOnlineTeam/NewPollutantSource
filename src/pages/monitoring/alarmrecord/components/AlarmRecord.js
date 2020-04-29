0f2a2384ad7ca3d143cd64d496e4805f90830aa1
xuepengyu <15210415451@163.com>
2020-04-29 15:07:21 +0800

    update

--- src/pages/monitoring/alarmrecord/components/AlarmRecord.js
+++ src/pages/monitoring/alarmrecord/components/AlarmRecord.js
@@ -369,16 +369,16 @@ class AlarmRecord extends Component {
 
     // },
     {
-      title: '核实状态',
+      title: '处置状态',
       width: 100,
       dataIndex: 'State',
       key: 'State',
 
       render: (text, record) => {
         if (text === '0') {
-          return <span> <Badge status="error" text="未核实" /> </span>;
+          return <span> <Badge status="error" text="未处置" /> </span>;
         }
-        return <span> <Badge status="default" text="已核实" /> </span>;
+        return <span> <Badge status="default" text="已处置" /> </span>;
       },
       // filters: [{
       //   text: '未核实',
@@ -439,7 +439,7 @@ class AlarmRecord extends Component {
                 dateValue={this.state.rangeDate}
                 callback={(dates) => this._handleDateChange(dates)}
               />
-              <Button style={{ marginTop: 5 }} onClick={this.BtnVerify}><Icon type="setting" theme="twoTone" />核实</Button>
+              <Button style={{ marginTop: 5 }} onClick={this.BtnVerify}><Icon type="setting" theme="twoTone" />处置</Button>
             </div>
           }
 
@@ -482,7 +482,7 @@ class AlarmRecord extends Component {
           </Card.Grid>
 
           <Modal
-            title="核实单详情"
+            title="处置单详情"
             visible={this.state.visible}
             destroyOnClose // 清除上次数据
             onOk={this.handleOk}
@@ -499,13 +499,13 @@ class AlarmRecord extends Component {
             <SdlForm configId="ExceptionVerify" form={this.props.form} hideBtns >
               <Row>
                 <Col span={12}>
-                  <FormItem {...formLayout} label="核实人">
+                  <FormItem {...formLayout} label="处置人">
                     {getFieldDecorator('VerifyPerSon1', {
                       initialValue: UserName,
                       rules: [
                         {
                           required: true,
-                          message: '核实人不能为空',
+                          message: '处置人不能为空',
                         },
                       ],
                     })(
@@ -514,13 +514,13 @@ class AlarmRecord extends Component {
                   </FormItem>
                 </Col>
                 <Col span={12}>
-                  <FormItem {...formLayout} label="核实时间">
+                  <FormItem {...formLayout} label="处置时间">
                     {getFieldDecorator('VerifyTime1', {
                       initialValue: moment(),
                       rules: [
                         {
                           required: true,
-                          message: '核实时间不能为空',
+                          message: '处置时间不能为空',
                         },
                       ],
                     })(
