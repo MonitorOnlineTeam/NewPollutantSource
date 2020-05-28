1128c792342041a4d063b75cf5505d1573e908ae
dongxiaoyun123 <1227906469@qq.com>
2020-05-28 13:20:24 +0800

    update

---
+++ src/pages/OperationSysManager/SparePartsStation/services.js
@@ -0,0 +1,36 @@
+/**
+ * 功  能：服务站信息services
+ * 创建人：dongxiaoyun
+ * 创建时间：2020.05.26
+ */
+import { post, get } from '@/utils/request';
+
+
+// 获取所有用户
+export async function GetAllOperationUsers(params) {
+  const result = get('/api/rest/PollutantSourceApi/SparepartManageApi/GetAllOperationUsers', params, null);
+  return result === null ? {
+      data: null
+  } : result;
+}
+
+// 获取服务站信息
+export async function getSparePartsStationList(params) {
+  const result = get('/api/rest/PollutantSourceApi/SparepartManageApi/getSparePartsStationList', params, null);
+  return result === null ? {
+      data: null
+  } : result;
+}
+
+
+// 保存用户信息
+export async function saveSparePartsStationUser(params) {
+  const result = post('/api/rest/PollutantSourceApi/SparepartManageApi/saveSparePartsStationUser', params, null);
+  return result === null ? {
+      data: null
+  } : result;
+}
+
+
+
+
