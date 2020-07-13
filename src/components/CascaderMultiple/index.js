34a016efdd747bf3bb22a45093e0f3010b438e74
Jiaqi <wangjq@polysoft.com.cn>
2020-07-13 09:05:36 +0800

    下拉多选组件新增行政区筛选

--- src/components/CascaderMultiple/index.js
+++ src/components/CascaderMultiple/index.js
@@ -51,17 +51,17 @@ class CascaderMultiple extends PureComponent {
     })
 
     if (!this.props.options) {
-      this.getDataList(this.props.pollutantTypes)
+      this.getDataList(this.props.pollutantTypes, this.props.regionCode)
     }
   }
 
 
-  getDataList = (pollutantTypes) => {
+  getDataList = (pollutantTypes, RegionCode) => {
     this.props.dispatch({
       type: "common/getEntAndPointList",
       payload: {
         "PollutantTypes": pollutantTypes,
-        "RegionCode": "",
+        "RegionCode": RegionCode,
         "Name": "",
         "Status": [0, 1, 2, 3],
         "QCAUse": "",
@@ -169,8 +169,8 @@ class CascaderMultiple extends PureComponent {
     }
 
     // 传入的污染物类型发生变化，重新请求数据
-    if (this.props.pollutantTypes !== nextProps.pollutantTypes) {
-      this.getDataList(nextProps.pollutantTypes)
+    if (this.props.pollutantTypes !== nextProps.pollutantTypes || this.props.regionCode !== nextProps.regionCode) {
+      this.getDataList(nextProps.pollutantTypes, nextProps.regionCode)
     }
 
     // if (this.props.entAndPointList !== nextProps.entAndPointList) {
