1a9caf6ef869c1a8a614aa8505aa04c0f24fe9fb
Jiaqi <wangjq@polysoft.com.cn>
2020-08-06 14:22:34 +0800

    修改

--- src/pages/dataSearch/monitor/working.js
+++ src/pages/dataSearch/monitor/working.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import BreadcrumbWrapper from "@/components/BreadcrumbWrapper"
+import { Card } from "antd"
 
 class working extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = {};
   }
   render() {
     return (
       <BreadcrumbWrapper>
+        <Card>
+          12312312312
+        </Card>
       </BreadcrumbWrapper>
     );
   }
