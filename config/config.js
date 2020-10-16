73fa20068170bb805b9548fb6f14598249587d23
humengdi <18225990227@163.com>
2020-10-16 17:59:36 +0800

    超标数据查询

--- config/config.js
+++ config/config.js
@@ -1316,6 +1316,11 @@ export default {
                   path: '/dataSearch/airStation',
                   component: './dataSearch/airStation',
                 },
+                {
+                  name: 'exceedData', //超标数据查询
+                  path: '/dataSearch/exceedData',
+                  component: './dataSearch/exceedData',
+                },
               ]
             },
             {
