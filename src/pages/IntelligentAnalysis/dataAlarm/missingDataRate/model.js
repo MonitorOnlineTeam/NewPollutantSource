649559cb9c129a6552ccf1ad2edbb59ef24e7107
jia_anbo <jia_anbo@163.com>
2020-11-25 15:53:38 +0800

    更新代码

--- src/pages/IntelligentAnalysis/dataAlarm/missingDataRate/model.js
+++ src/pages/IntelligentAnalysis/dataAlarm/missingDataRate/model.js
@@ -60,7 +60,7 @@ export default Model.extend({
           regionCode:'',
           entCount:entCount,
           pointCount:pointCount,
-          responseRate:(xiangyingCount/exceptionCount)*100,
+          responseRate:Number(xiangyingCount)/Number(exceptionCount)*100,
           exceptionCount:exceptionCount,
           weixiangyingCount:weixiangyingCount,
           xiangyingCount:xiangyingCount
