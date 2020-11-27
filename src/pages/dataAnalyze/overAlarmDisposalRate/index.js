d93c6e4050c4ec50411a08e56bd5242f2e8fb510
markm4 <houxfmark3955@163.com>
2020-11-27 17:31:55 +0800

    修改bug

--- src/pages/dataAnalyze/overAlarmDisposalRate/index.js
+++ src/pages/dataAnalyze/overAlarmDisposalRate/index.js
@@ -2,7 +2,7 @@
  * @Description:超标报警处置率-一级
  * @LastEditors: hxf
  * @Date: 2020-10-16 16:16:39
- * @LastEditTime: 2020-11-27 17:03:34
+ * @LastEditTime: 2020-11-27 17:31:01
  * @FilePath: /NewPollutantSource/src/pages/dataAnalyze/overAlarmDisposalRate/index.js
  */
 
@@ -444,12 +444,13 @@ class index extends PureComponent {
                   )}
                 </FormItem>
               </Col>
-              {/* <Col md={5}>
+              <Col md={5}>
                 <FormItem {...formLayout} label="关注程度" style={{ width: '100%' }}>
                   {getFieldDecorator('AttentionCode', {
                     initialValue: AttentionCode,
                   })(
                     <Select
+                      allowClear
                       placeholder="请选择关注程度"
                       onChange={value => {
                         this.setState({ AttentionCode: value }, () => { });
@@ -461,7 +462,7 @@ class index extends PureComponent {
                         });
                       }}
                     >
-                      <Option value="">全部</Option>
+                      <Option value=""></Option>
                       {attentionList.map(item => {
                         return (
                           <Option key={item.AttentionCode} value={item.AttentionCode}>
@@ -472,7 +473,7 @@ class index extends PureComponent {
                     </Select>,
                   )}
                 </FormItem>
-              </Col> */}
+              </Col>
             </Row>
             <Row gutter={24}>
               <Col md={4} style={{ marginTop: 10 }}>
