76c5859e81ab1def831e4eded988cc0ee456cc4e
Jiaqi <wjiaqi7@163.com>
2021-07-30 11:31:28 +0800

    修改bug

--- config/router.config.js
+++ config/router.config.js
@@ -1831,12 +1831,12 @@ const routes = [
                   },
                 ]
               },
-              // {
-              //   // 碳排放查询
-              //   name: 'CO2Emissions',
-              //   path: '/dataSearch/CO2Emissions',
-              //   component: './dataSearch/CO2Emissions',
-              // },
+              {
+                // 碳排放查询
+                name: 'CO2Emissions',
+                path: '/dataSearch/CO2Emissions',
+                component: './dataSearch/CO2Emissions',
+              },
               {
                 // 站点数据查询
                 path: '/dataSearch/siteData',
