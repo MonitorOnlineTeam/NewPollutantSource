ad1b33bc019fb32b0b0fa74453893a9fe1b97ff1
Jiaqi <wjiaqi7@163.com>
2022-04-18 11:31:12 +0800

    修改历史管控参数页面bug

--- src/pages/dynamicControl/controlData/historyparame/index.jsx
+++ src/pages/dynamicControl/controlData/historyparame/index.jsx
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import BreadcrumbWrapper from "@/components/BreadcrumbWrapper"
 import PageLoading from '@/components/PageLoading'
-import  HistoryparData from './components/HistoryparData'
+import HistoryparData from './components/HistoryparData'
 
 import NavigationTree from '@/components/NavigationTreeNew'
 
@@ -16,41 +16,45 @@ class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dgimn: props.location.query&&props.location.query.type==="alarm"?props.location.query.dgimn:"",
+            dgimn: props.location.query && props.location.query.type === "alarm" ? props.location.query.dgimn : "",
             title: '',
         };
     }
 
-    changeDgimn = (value, selectItem)=> {
+    changeDgimn = (value, selectItem) => {
         this.setState({
             dgimn: value,
             title: selectItem.title,
         })
     }
-   componentDidMount(){
-       const { location } = this.props;
-       if(location.query&&location.query.type==='alarm' ){
-        this.setState({
-            title: location.query.title,
-        })  
-       }
-   }
+    componentDidMount() {
+        const { location } = this.props;
+        if (location.query && location.query.type === 'alarm') {
+            this.setState({
+                title: location.query.title,
+            })
+        }
+    }
     render() {
-        const { dgimn,title } = this.state;
+        const { dgimn, title } = this.state;
 
         const { location } = this.props;
-        
+        console.log('dgimn=', dgimn);
         return (
             <div id='historyparData'>
-              {location.query&&location.query.type==='alarm' ? null :  <NavigationTree onItemClick={(value,selectItem) => {  this.changeDgimn(value,selectItem) }} /> }
-              {/* <NavigationTree onItemClick={(value,selectItem) => {  this.changeDgimn(value,selectItem) }} /> */}
-              {location.query&&location.query.type==='alarm' ?
-                <HistoryparData  dgimn={location.query.dgimn} initLoadData location={this.props.location}/>
-                :
-                <BreadcrumbWrapper extraName={ `${ title}`}>
-                { dgimn?    <HistoryparData dgimn={dgimn} initLoadData location={this.props.location}/> : <PageLoading /> } 
-                </BreadcrumbWrapper> 
-               }
+                {location.query && location.query.type === 'alarm' ? null :
+                    <NavigationTree onTreeSelect={(value, selectItem) => {
+                        this.changeDgimn(value, selectItem)
+                    }} />
+                }
+                {/* <NavigationTree onItemClick={(value,selectItem) => {  this.changeDgimn(value,selectItem) }} /> */}
+                {location.query && location.query.type === 'alarm' ?
+                    <HistoryparData dgimn={location.query.dgimn} initLoadData location={this.props.location} />
+                    :
+                    <BreadcrumbWrapper extraName={`${title}`}>
+                        {dgimn ? <HistoryparData dgimn={dgimn} initLoadData location={this.props.location} /> : <PageLoading />}
+                    </BreadcrumbWrapper>
+                }
             </div>
         );
     }
