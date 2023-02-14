3ce8abd7301cbd5909279c6ad4010c9021fa9721
JiaQi <15398829@qq.com>
2023-02-14 10:04:54 +0800

    1

---
+++ src/pages/home/yanshi/DataFolwChart.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Realtimedata from '@/pages/monitoring/realtimedata'
+import Cookie from 'js-cookie';
+
+
+class DataFolwChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.match.params.data
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    console.log('props', this.props)
+    const { data } = this.state;
+    const { MN, vertical, scale } = this.props.location.query;
+    console.log('vertical', vertical)
+    return <>
+      {/* <p>{Cookie.get('ssToken')}</p> */}
+      <Realtimedata showMode="modal"
+        vertical={vertical == 'true'}
+        wrapperStyle={{ height: '100vh' }}
+        scale={scale}
+        currentTreeItemData={
+          [{
+            // "key": "62030231rdep11",
+            "key": this.props.location.query.MN,
+            // "pointName": "北热1号分析小屋",
+            // "entName": "华能北京热电厂",
+            // "Type": "2",
+            // "EntCode": "7526121f-1229-44dd-9de1-429bf6654664",
+          }]
+        } />
+    </>
+  }
+}
+
+export default DataFolwChart;
