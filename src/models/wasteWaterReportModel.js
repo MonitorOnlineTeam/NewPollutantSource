3a74b0c853bf81072867722e96fa54c48f26606a
JiaQi <15398829@qq.com>
2023-02-17 15:03:29 +0800

    1

--- src/models/wasteWaterReportModel.js
+++ src/models/wasteWaterReportModel.js
@@ -4,7 +4,7 @@
  * 创建时间：2020.10.21
  */
 import Model from '@/utils/model';
-import {GetEntByRegionAndAtt,GetPointByEntCode,GetAllTypeDataListWater,ExportAllTypeDataListWater} from '../services/wasteWaterReportApi'
+import { GetEntByRegionAndAtt, GetPointByEntCode, GetAllTypeDataListWater, ExportAllTypeDataListWater, getReportColumns } from '../services/wasteWaterReportApi'
 import moment from 'moment';
 import { message } from 'antd';
 import { downloadFile } from '@/utils/utils';
@@ -17,80 +17,90 @@ export default Model.extend({
     PageSize: 10,
     PageIndex: 1,
     total: 0,
-    EntByRegionAndAttList:[],
-    PointByEntCodeList:[],
-    AllTypeDataListWaterList:[]
+    EntByRegionAndAttList: [],
+    PointByEntCodeList: [],
+    AllTypeDataListWaterList: []
   },
   subscriptions: {},
   effects: {
-      *GetEntByRegionAndAtt({ payload }, { call, put, update, select }) {
+    *GetEntByRegionAndAtt({ payload, callback }, { call, put, update, select }) {
 
-        const body = {
-          RegionCode:payload.RegionCode,
-          Attention:payload.Attention,
-          PollutantTypeCode:payload.PollutantTypeCode,
-        }
-      
-        const result = yield call(GetEntByRegionAndAtt, body, null)
-        if (result.IsSuccess) {
-          yield update({
-            EntByRegionAndAttList: result.Datas
-          })
-        }
-        else {
-          yield update({
-            EntByRegionAndAttList: []
-          })
-        }
-      },
-      *GetPointByEntCode({ payload }, { call, put, update, select }) {
+      const body = {
+        RegionCode: payload.RegionCode,
+        Attention: payload.Attention,
+        PollutantTypeCode: payload.PollutantTypeCode,
+      }
 
-        console.log(payload)
-        const result = yield call(GetPointByEntCode, payload, null)
-        console.log(result)
-        if (result.IsSuccess) {
-          yield update({
-            PointByEntCodeList: result.Datas
-          })
-        }
-        else {
-          yield update({
-            PointByEntCodeList: []
-          })
-        }
-      },
-      *GetAllTypeDataListWater({ payload }, { call, put, update, select }) {
-        const body ={
-          BeginTime:payload.BeginTime,
-          EndTime:payload.EndTime,
-          DGIMN:payload.DGIMN,
-          dataType:payload.dataType,
-          time:payload.time
-        }
-        const result = yield call(GetAllTypeDataListWater, body, null)
-        if (result.IsSuccess) {
-          yield update({
-            AllTypeDataListWaterList: result.Datas
-          })
-        }
-        else {
-          yield update({
-            AllTypeDataListWaterList: []
-          })
-        }
-      },
-      *ExportAllTypeDataListWater({ payload }, { call, put, update, select }) {
-        const body ={
-          BeginTime:payload.BeginTime,
-          EndTime:payload.EndTime,
-          DGIMN:payload.DGIMN,
-          dataType:payload.dataType,
-          time:payload.time
-        }
-        const result = yield call(ExportAllTypeDataListWater, body, null)
-        if (result.IsSuccess) {
-          downloadFile(`${result.Datas}`)
-        }
-      },
+      const result = yield call(GetEntByRegionAndAtt, body, null)
+      if (result.IsSuccess) {
+        callback && callback(result.Datas)
+        yield update({
+          EntByRegionAndAttList: result.Datas
+        })
+      }
+      else {
+        yield update({
+          EntByRegionAndAttList: []
+        })
+      }
+    },
+    *GetPointByEntCode({ payload }, { call, put, update, select }) {
+
+      console.log(payload)
+      const result = yield call(GetPointByEntCode, payload, null)
+      console.log(result)
+      if (result.IsSuccess) {
+        yield update({
+          PointByEntCodeList: result.Datas
+        })
+      }
+      else {
+        yield update({
+          PointByEntCodeList: []
+        })
+      }
+    },
+    *GetAllTypeDataListWater({ payload }, { call, put, update, select }) {
+      const body = {
+        BeginTime: payload.BeginTime,
+        EndTime: payload.EndTime,
+        DGIMN: payload.DGIMN,
+        dataType: payload.dataType,
+        time: payload.time
+      }
+      const result = yield call(GetAllTypeDataListWater, body, null)
+      if (result.IsSuccess) {
+        yield update({
+          AllTypeDataListWaterList: result.Datas
+        })
+      }
+      else {
+        yield update({
+          AllTypeDataListWaterList: []
+        })
+      }
+    },
+    *ExportAllTypeDataListWater({ payload }, { call, put, update, select }) {
+      const body = {
+        BeginTime: payload.BeginTime,
+        EndTime: payload.EndTime,
+        DGIMN: payload.DGIMN,
+        dataType: payload.dataType,
+        time: payload.time
+      }
+      const result = yield call(ExportAllTypeDataListWater, body, null)
+      if (result.IsSuccess) {
+        downloadFile(`${result.Datas}`)
+      }
+    },
+    // 获取报表表头
+    *getReportColumns({ payload, callback }, { call, put, update, select }) {
+      const result = yield call(getReportColumns, payload)
+      if (result.IsSuccess) {
+        callback && callback(result.Datas)
+      } else {
+        message.error(result.Message)
+      }
+    },
   },
 });
