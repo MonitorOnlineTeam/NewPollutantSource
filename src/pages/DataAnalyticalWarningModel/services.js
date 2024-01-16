0444fd4d8144539e98f3cc9b41adc4232a7ab1c7
JiaQi <15398829@qq.com>
2024-01-16 15:46:58 +0800

    1

--- src/pages/DataAnalyticalWarningModel/services.js
+++ src/pages/DataAnalyticalWarningModel/services.js
@@ -1,4 +1,5 @@
 import { post } from '@/utils/request';
+import { API } from '@config/API';
 
 // 获取模型列表
 export async function GetModelList(params) {
@@ -119,10 +120,7 @@ export async function StatisLinearCoefficient(params) {
 
 // 根据企业获取排口
 export async function getPointByEntCode(params) {
-  const result = await post(
-    '/api/rest/PollutantSourceApi/BaseDataApi/GetNoFilterPointByEntCode',
-    params,
-  );
+  const result = await post(API.CommonApi.GetNoFilterPointByEntCode, params);
   return result;
 }
 
@@ -203,10 +201,7 @@ export async function ExportStatisAlarm(params) {
 
 // 根据企业获取排口
 export async function GetNoFilterPointByEntCode(params) {
-  const result = await post(
-    '/api/rest/PollutantSourceApi/BaseDataApi/GetNoFilterPointByEntCode',
-    params,
-  );
+  const result = await post(API.CommonApi.GetNoFilterPointByEntCode, params);
   return result;
 }
 
