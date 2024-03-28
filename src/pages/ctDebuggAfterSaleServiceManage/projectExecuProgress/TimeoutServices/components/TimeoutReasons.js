49514549b84a3ea0da6add632df1c1e7194109ef
JiaQi <wjiaqi7@163.com>
2024-03-28 16:05:15 +0800

    修改文字bug

--- src/pages/ctDebuggAfterSaleServiceManage/projectExecuProgress/TimeoutServices/components/TimeoutReasons.js
+++ src/pages/ctDebuggAfterSaleServiceManage/projectExecuProgress/TimeoutServices/components/TimeoutReasons.js
@@ -2,7 +2,7 @@
  * @Author: JiaQi
  * @Date: 2024-03-25 15:30:21
  * @Last Modified by: JiaQi
- * @Last Modified time: 2024-03-26 17:20:52
+ * @Last Modified time: 2024-03-28 15:47:52
  * @Description:  超时原因
  */
 import React, { useState, useEffect, useMemo } from 'react';
@@ -182,7 +182,7 @@ const TimeoutReasons = props => {
   }, [TimeoutReasonAnalysis, echarts]);
 
   return (
-    <Card title="超时原因（小时）" size="small" bodyStyle={{ height: 300 }} loading={loading}>
+    <Card title="超时原因时长（小时）" size="small" bodyStyle={{ height: 300 }} loading={loading}>
       {renderEcharts}
     </Card>
   );
