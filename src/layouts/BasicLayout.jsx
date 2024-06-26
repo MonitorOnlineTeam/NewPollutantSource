f6c87ee4bfd57c4578296f2ed5a460e45e61b690
outman0611 <jia_anbo@163.com>
2024-06-26 19:05:19 +0800

    更新代码

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -423,7 +423,7 @@ class BasicLayout extends Component {
     return (
       <>
         <ProLayout
-          logo={logoRender}
+          logo={logoRender}//运维不需要
           onCollapse={handleMenuCollapse}
           menuItemRender={(menuItemProps, defaultDom) => {
             if (menuItemProps.replace && userCookie !== 'null') {
@@ -453,8 +453,9 @@ class BasicLayout extends Component {
           rightContentRender={rightProps => <RightContent {...rightProps} />}
           {...this.props}
           {..._settings}
+          // menuHeaderRender={() => <a href={currentMenu?.[0]?.path}> <h1>{configInfo && configInfo.SystemName}</h1></a>} //运维
           // title={configInfo && configInfo.SystemName}
-          // menuHeaderRender={() => <a href={currentMenu?.[0]?.path}> <h1>{configInfo && configInfo.SystemName}</h1></a>}
+         
         >
           {
             config.isShowTabs && defaultSettings.layout === 'sidemenu' ? <div id="sideMenuTabsLayout" style={{ margin: '-24px -24px 0px', padding: '10px', paddingTop: 4 }}><Tabs
