1163d230367768918b8dfb3904cfc12a2923268f
outman0611 <jia_anbo@163.com>
2024-07-05 17:52:56 +0800

    更新代码

--- src/pages/workSupervision/dailyManagement/customerReturnVisit/index.js
+++ src/pages/workSupervision/dailyManagement/customerReturnVisit/index.js
@@ -270,7 +270,7 @@ const ReturnVisit = props => {
                 >
                   导出
                 </Button>
-                {/* {buttonList.includes('officeManagement') && ( */}
+                {buttonList.includes('customerSitefollowManagement') && (
                 <Button
                   type="primary"
                   onClick={() => {
@@ -280,7 +280,7 @@ const ReturnVisit = props => {
                 >
                   客户现场回访管理
                 </Button>
-                {/* )} */}
+                 )} 
                 <Button
                   type="primary"
                   onClick={() => {
