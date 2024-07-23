10f314102670ecc638e298f65902f75ccaf02c8a
JiaQi <wjiaqi7@163.com>
2024-07-23 11:15:37 +0800

    1

--- config/config.js
+++ config/config.js
@@ -13,10 +13,11 @@ import path from 'path';
 // const API_HOST = 'http://60.29.13.132:60061';  // 60
 // const API_HOST = 'http://172.16.12.60:6001/';  // 60
 // const API_HOST = 'http://172.16.12.134:61003/';  // 运维正式
-// const API_HOST = 'http://172.16.12.39:49003/';  // 234 运维测试
+// const API_HOST = 'http://172.16.12.39:49003/'; // 234 运维测试
 // const API_HOST = 'http://172.16.12.91:61007/' //宝武集团 正式
 const API_HOST = 'http://172.16.12.134:61005/'; //134 - 演示
 const API_HOST2 = 'http://172.16.12.109:61001/'; //调试服务导出PDF api
+const UPLOAD_HOST = 'http://172.16.12.134:61001/'; // 文件上传地址
 
 const { pwa, primaryColor } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
@@ -149,20 +150,18 @@ export default {
     '/rest': {
       target: API_HOST,
       changeOrigin: true,
-      // pathRewrite: { '^/api': '' },
     },
     '/wwwroot': {
       target: API_HOST, // 接口的域名
-      changeOrigin: true, //
-      // pathRewrite: { '^/wwwroot': '' }, // pathRewrite 来重写地址，将前缀 '/api' 转为 '/'。
+      changeOrigin: true,
     },
     '=/wwwroot/Upload/': {
-      target: 'http://172.16.12.134:61001/', // 接口的域名
-      changeOrigin: true, //
+      target: UPLOAD_HOST, // 接口的域名
+      changeOrigin: true,
     },
     '~* /.*UploadApi/': {
-      target: 'http://172.16.12.134:61001/', // 接口的域名
-      changeOrigin: true, //
+      target: UPLOAD_HOST, // 文件上传地址
+      changeOrigin: true,
     },
     '/testPdfApi': {
       target: API_HOST2, //调试服务导出PDF 代理
